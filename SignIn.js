ce5bc4bd26b88f847fbb2b2aebddc5459942ae05
vishal <vishal88kh@gmail.com>
2023-08-16 01:55:29 +0530

    added sign up form functionlity

---
+++ SignIn.js
@@ -0,0 +1,31 @@
+const signInform = document.querySelector("#sign-in-form");
+console.log(signInform);
+const signInButton  = document.querySelector("#signInButton");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const registerMessage = document.querySelector(".register-Message")
+console.log(signInButton);
+console.log(emailInput);
+console.log(passwordInput);
+
+fetch("http://localhost:3000/users")
+.then(reponse =>reponse.json())
+.then(userData=>{
+    console.log(userData);
+    signInButton.addEventListener('click',function(){
+        const email =  emailInput.value;
+        const password = passwordInput.value;
+        const user =  userData.find(user =>user.email===email && user.password===password);
+
+        if(user){
+            window.location.href="ContactList.html";
+        }else{
+            registerMessage.style.display="flex";
+        }
+    })
+})
+.catch(error=>{
+    console.error("Error : ",error);
+})
+
+
