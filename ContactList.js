d78f19ba1993f70eba2abb17f41bf87b93312fdf
vishal <vishal88kh@gmail.com>
2023-08-16 01:16:02 +0530

    added a lines of code which will featch the latest id from the json file

--- ContactList.js
+++ ContactList.js
@@ -1,12 +1,16 @@
 const form = document.querySelector("#Sign-up-Form")
 console.log(form);
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', async function(event){
     event.preventDefault();
     const email =  document.querySelector("#email").value;
     const password = document.querySelector("#password").value;
     const secret = document.querySelector("#sceret").value;
-
+    const latestIdResponse = await fetch('http://localhost:3000/users?_sort=id&_order=desc&_limit=1');    
+    const latestIdData = await latestIdResponse.json();
+    const latestId = latestIdData.length > 0 ? latestIdData[0].id : 0;
+    const newId = latestId + 1;
     const data = {
+        id:newId,
         email:email,
         password:password,
         secret:secret,
@@ -15,19 +19,20 @@ form.addEventListener('submit', function(event){
         }]
         
     }
-    fetch("http://localhost:3000")
-
-    const jsonData = JSON.stringify(data,null,2);
-
-    const saveDataToFile = () => {
-        return new Promise((resolve, reject) => {
-            resolve(jsonData);
-        },1000);
-    }
-
-    saveDataToFile().then(savedData=>{
-        console.log("Data saved : ",savedData);
-    }).catch(error=>{
+    fetch("http://localhost:3000/users",{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+    .then(response =>response.text())
+    .then(data =>{
+        console.log(data);
+        window.location.href="SignIn.html";
+    })
+    .catch(error=>{
         console.error("Error : ",error);
     })
+    
 });
