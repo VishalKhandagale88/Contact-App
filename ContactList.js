5d5e93cb342d1fc6a7681a20bb5c92639172ee74
vishal <vishal88kh@gmail.com>
2023-08-16 00:35:07 +0530

    added function to get email, password and secret vlues

--- ContactList.js
+++ ContactList.js
@@ -2,5 +2,8 @@ const form = document.querySelector("#Sign-up-Form")
 console.log(form);
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    
+    const email =  document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    const secret = document.querySelector("#sceret");
+    console.log(email + "  " +password +" "+ secret);
 });
